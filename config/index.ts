3410afb375c1b339fd37aaddf1c9f032eafcf484
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-02-20 10:40:10 -0500

    new commenting commit

--- config/index.ts
+++ config/index.ts
@@ -1,6 +1,13 @@
-export const CURRENCY = 'usd'
-// Set your amount limits: Use float for decimal currencies and
-// Integer for zero-decimal currencies: https://stripe.com/docs/currencies#zero-decimal.
-export const MIN_AMOUNT = 10.0
-export const MAX_AMOUNT = 5000.0
-export const AMOUNT_STEP = 5.0
+// Set the currency to be used for transactions. In this case, it is US Dollars (USD).
+export const CURRENCY = 'usd';
+
+// Set the minimum amount limit for transactions. This value is in decimal form,
+// which is suitable for currencies with decimal values such as USD.
+export const MIN_AMOUNT = 10.0;
+
+// Set the maximum amount limit for transactions. This value is also in decimal form.
+export const MAX_AMOUNT = 5000.0;
+
+// Set the amount step for transactions. This value is also in decimal form.
+// It represents the amount by which the transaction amount will increase or decrease.
+export const AMOUNT_STEP = 5.0;
