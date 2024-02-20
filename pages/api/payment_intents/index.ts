e5e26619c9bd72b80865dae236dbb3630ab51f6f
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-02-20 11:22:48 -0500

    new refactor

--- pages/api/payment_intents/index.ts
+++ pages/api/payment_intents/index.ts
@@ -1,39 +1,45 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-
 import { CURRENCY, MIN_AMOUNT, MAX_AMOUNT } from '../../../config';
 import { formatAmountForStripe } from '../../../utils/stripe-helpers';
-
 import Stripe from 'stripe';
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  // https://github.com/stripe/stripe-node#configuration
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: '2020-03-02',
 });
+
+interface BodyParams {
+  amount: number;
+}
+
+interface PaymentIntentCreateParams extends Stripe.PaymentIntentCreateParams {
+  amount: number;
+  currency: string;
+}
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === 'POST') {
-    const { amount }: { amount: number } = req.body;
-    try {
-      // Validate the amount that was passed from the client.
-      if (!(amount >= MIN_AMOUNT && amount <= MAX_AMOUNT)) {
-        throw new Error('Invalid amount.');
-      }
-      // Create PaymentIntent from body params.
-      const params: Stripe.PaymentIntentCreateParams = {
-        payment_method_types: ['card'],
-        amount: formatAmountForStripe(amount, CURRENCY),
-        currency: CURRENCY,
-      };
-      const payment_intent: Stripe.PaymentIntent = await stripe.paymentIntents.create(
-        params
-      );
+    const { amount }: BodyParams = req.body;
 
-      res.status(200).json(payment_intent);
-    } catch (err) {
-      res.status(500).json({ statusCode: 500, message: err.message });
+    // Validate the amount that was passed from the client.
+    if (!(amount >= MIN_AMOUNT && amount <= MAX_AMOUNT)) {
+      throw new Error('Invalid amount.');
     }
+
+    // Create PaymentIntent from body params.
+    const params: PaymentIntentCreateParams = {
+      payment_method_types: ['card'],
+      amount: formatAmountForStripe(amount, CURRENCY),
+      currency: CURRENCY,
+    };
+
+    const paymentIntent: Stripe.PaymentIntent = await stripe.paymentIntents.create(
+      params
+    );
+
+    res.status(200).json(paymentIntent);
   } else {
     res.setHeader('Allow', 'POST');
     res.status(405).end('Method Not Allowed');
