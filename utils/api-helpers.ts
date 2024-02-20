f34bd6932dca14f9aecddba727cfcbb0d6fda43a
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-02-20 11:22:26 -0500

    new refactor

--- utils/api-helpers.ts
+++ utils/api-helpers.ts
@@ -1,29 +1,31 @@
-export async function fetchGetJSON(url: string) {
+export async function fetchGetJSON(url: string): Promise<any> {
   try {
-    const data = await fetch(url).then((res) => res.json());
+    const response = await fetch(url);
+    const data = await response.json();
     return data;
   } catch (err) {
     throw new Error(err.message);
   }
 }
 
-export async function fetchPostJSON(url: string, data?: {}) {
+export async function fetchPostJSON(url: string, data?: {}): Promise<any> {
   try {
-    // Default options are marked with *
-    const response = await fetch(url, {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'same-origin', // include, *same-origin, omit
+    const defaultOptions = {
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
       headers: {
         'Content-Type': 'application/json',
-        // 'Content-Type': 'application/x-www-form-urlencoded',
       },
-      redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'no-referrer', // no-referrer, *client
-      body: JSON.stringify(data || {}), // body data type must match "Content-Type" header
-    });
-    return await response.json(); // parses JSON response into native JavaScript objects
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+      body: JSON.stringify(data || {}),
+    };
+
+    const response = await fetch(url, defaultOptions);
+    const jsonData = await response.json();
+    return jsonData;
   } catch (err) {
     throw new Error(err.message);
   }
