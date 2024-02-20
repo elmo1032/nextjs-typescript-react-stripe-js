5113b9baf3e95b6a6508222358aa28ec3c17ec36
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-02-20 10:40:15 -0500

    new commenting commit

--- utils/api-helpers.ts
+++ utils/api-helpers.ts
@@ -1,30 +1,34 @@
-export async function fetchGetJSON(url: string) {
+// This function exports an async function named fetchGetJSON which takes in a URL as a string and an optional parameter g.
+// It is used to fetch data from a given URL and return the result as JSON.
+export async function fetchGetJSON(url: string, g) {
   try {
+    // The fetch function is used to make a request to the URL. The response is then converted to JSON format.
     const data = await fetch(url).then((res) => res.json());
+    // The resolved data is returned.
     return data;
   } catch (err) {
+    // If there's an error, a new Error object is thrown with the error message.
     throw new Error(err.message);
   }
 }
 
-export async function fetchPostJSON(url: string, data?: {}) {
+// This function exports an async function named fetchPostJSON which takes in a URL as a string and an optional data object.
+// It is used to send a POST request to the given URL with the provided data and return the response as JSON.
+export async function fetchPostJSON(url: string, data = {}) {
   try {
-    // Default options are marked with *
+    // Default options for the fetch request are defined here.
     const response = await fetch(url, {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'same-origin', // include, *same-origin, omit
+      method: 'POST', // The request method is set to POST.
+      mode: 'cors', // The mode is set to cors to allow cross-origin requests.
+      cache: 'no-cache', // The cache is set to no-cache to prevent using a cached response.
+      credentials: 'same-origin', // The credentials are set to same-origin to send cookies only to the same origin.
       headers: {
-        'Content-Type': 'application/json',
-        // 'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Type': 'application/json', // The Content-Type header is set to application/json to indicate JSON data.
       },
-      redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'no-referrer', // no-referrer, *client
-      body: JSON.stringify(data || {}), // body data type must match "Content-Type" header
+      redirect: 'follow', // The redirect policy is set to follow to automatically follow redirects.
+      referrerPolicy: 'no-referrer', // The referrer policy is set to no-referrer to send no referrer information.
+      body: JSON.stringify(data), // The request body is set to the JSON-stringified data object.
     });
-    return await response.json(); // parses JSON response into native JavaScript objects
-  } catch (err) {
-    throw new Error(err.message);
+    // The response is converted to JSON format and returned.
+    return await response.json();
   }
-}
