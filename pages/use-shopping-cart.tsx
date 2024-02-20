3c8707b60010ab1e2debb626e02c96705cf6d3ee
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-02-20 11:22:14 -0500

    new refactor

--- pages/use-shopping-cart.tsx
+++ pages/use-shopping-cart.tsx
@@ -1,6 +1,5 @@
 import { NextPage } from 'next';
 import Layout from '../components/Layout';
-
 import Cart from '../components/Cart';
 import CartSummary from '../components/CartSummary';
 import Products from '../components/Products';
