44d83e483654b297fe698949b303d7b5a3b00e02
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-02-20 11:22:32 -0500

    new refactor

--- pages/api/checkout_sessions/index.ts
+++ pages/api/checkout_sessions/index.ts
@@ -1,13 +1,15 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-
 import { CURRENCY, MIN_AMOUNT, MAX_AMOUNT } from '../../../config';
 import { formatAmountForStripe } from '../../../utils/stripe-helpers';
-
 import Stripe from 'stripe';
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  // https://github.com/stripe/stripe-node#configuration
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: '2020-03-02',
 });
+
+interface CheckoutSessionParams extends Stripe.Checkout.SessionCreateParams {
+  amount: number;
+}
 
 export default async function handler(
   req: NextApiRequest,
@@ -15,13 +17,13 @@ export default async function handler(
 ) {
   if (req.method === 'POST') {
     const amount: number = req.body.amount;
+
     try {
-      // Validate the amount that was passed from the client.
-      if (!(amount >= MIN_AMOUNT && amount <= MAX_AMOUNT)) {
-        throw new Error('Invalid amount.');
-      }
-      // Create Checkout Sessions from body params.
-      const params: Stripe.Checkout.SessionCreateParams = {
+      validateAmount(amount);
+
+      const params: CheckoutSessionParams = {
+        amount: formatAmountForStripe(amount, CURRENCY),
+        currency: CURRENCY,
         submit_type: 'donate',
         payment_method_types: ['card'],
         line_items: [
@@ -35,9 +37,8 @@ export default async function handler(
         success_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${req.headers.origin}/donate-with-checkout`,
       };
-      const checkoutSession: Stripe.Checkout.Session = await stripe.checkout.sessions.create(
-        params
-      );
+
+      const checkoutSession: Stripe.Checkout.Session = await stripe.checkout.sessions.create(params);
 
       res.status(200).json(checkoutSession);
     } catch (err) {
@@ -46,5 +47,11 @@ export default async function handler(
   } else {
     res.setHeader('Allow', 'POST');
     res.status(405).end('Method Not Allowed');
+  }
+}
+
+function validateAmount(amount: number) {
+  if (!(amount >= MIN_AMOUNT && amount <= MAX_AMOUNT)) {
+    throw new Error('Invalid amount.');
   }
 }
