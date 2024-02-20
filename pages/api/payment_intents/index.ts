79ff9ef2daa1ff98c6b6e7350174eab01594e3e8
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-02-20 10:40:14 -0500

    new commenting commit

--- pages/api/payment_intents/index.ts
+++ pages/api/payment_intents/index.ts
@@ -1,11 +1,13 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
+// Importing the necessary constants and helper functions
 import { CURRENCY, MIN_AMOUNT, MAX_AMOUNT } from '../../../config';
 import { formatAmountForStripe } from '../../../utils/stripe-helpers';
 
+// Importing the Stripe library and initializing it with the secret key
 import Stripe from 'stripe';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  // https://github.com/stripe/stripe-node#configuration
+  // Configuring the Stripe library with the specified API version
   apiVersion: '2020-03-02',
 });
 
@@ -13,28 +15,36 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  // Only processing POST requests
   if (req.method === 'POST') {
-    const { amount }: { amount: number } = req.body;
+    const { amount } = req.body; // Extracting the amount from the request body
+
     try {
-      // Validate the amount that was passed from the client.
+      // Validating the amount
       if (!(amount >= MIN_AMOUNT && amount <= MAX_AMOUNT)) {
         throw new Error('Invalid amount.');
       }
-      // Create PaymentIntent from body params.
+
+      // Creating the PaymentIntent parameters object
       const params: Stripe.PaymentIntentCreateParams = {
         payment_method_types: ['card'],
         amount: formatAmountForStripe(amount, CURRENCY),
         currency: CURRENCY,
       };
+
+      // Creating the PaymentIntent with the Stripe library
       const payment_intent: Stripe.PaymentIntent = await stripe.paymentIntents.create(
         params
       );
 
+      // Returning the PaymentIntent as a JSON response
       res.status(200).json(payment_intent);
     } catch (err) {
+      // Returning an error response in case of any errors
       res.status(500).json({ statusCode: 500, message: err.message });
     }
   } else {
+    // Returning a 405 Method Not Allowed error for any requests other than POST
     res.setHeader('Allow', 'POST');
     res.status(405).end('Method Not Allowed');
   }
