b9ad149f4a658b027cea0abb47e3f1d19d7767b0
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-02-20 10:40:09 -0500

    new commenting commit

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,9 @@
+// This is a TypeScript file which uses Next.js framework
+
 /// <reference types='next' />
+// This line is a reference to the Next.js framework, which provides
+// the necessary types for using Next.js in this TypeScript file.
+
 /// <reference types='next/types/global' />
+// This line is a reference to the global types provided by Next.js,
+// which includes types for NextPage, NextPageContext, and AppProps.
