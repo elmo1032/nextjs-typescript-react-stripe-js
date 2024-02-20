c475594639630b39080f60d6a8c5c9abc63436c4
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-02-20 11:23:07 -0500

    new refactor

--- components/CheckoutForm.tsx
+++ components/CheckoutForm.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
-
 import CustomDonationInput from '../components/CustomDonationInput';
 import StripeTestCards from '../components/StripeTestCards';
-
 import getStripe from '../utils/get-stripejs';
 import { fetchPostJSON } from '../utils/api-helpers';
 import { formatAmountForDisplay } from '../utils/stripe-helpers';
@@ -14,16 +12,16 @@ const CheckoutForm = () => {
     customDonation: Math.round(config.MAX_AMOUNT / config.AMOUNT_STEP),
   });
 
-  const handleInputChange: React.ChangeEventHandler<HTMLInputElement> = (e) =>
+  const handleInputChange = (e) => {
     setInput({
       ...input,
       [e.currentTarget.name]: e.currentTarget.value,
     });
+  };
 
-  const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
-    // Create a Checkout Session.
     const response = await fetchPostJSON('/api/checkout_sessions', {
       amount: input.customDonation,
     });
@@ -33,18 +31,15 @@ const CheckoutForm = () => {
       return;
     }
 
-    // Redirect to Checkout.
     const stripe = await getStripe();
     const { error } = await stripe!.redirectToCheckout({
-      // Make the id field from the Checkout Session creation API response
-      // available to this file, so you can provide it as parameter here
-      // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
       sessionId: response.id,
     });
-    // If `redirectToCheckout` fails due to a browser or network
-    // error, display the localized error message to your customer
-    // using `error.message`.
-    console.warn(error.message);
+
+    if (error) {
+      console.warn(error.message);
+    }
+
     setLoading(false);
   };
 
@@ -52,7 +47,7 @@ const CheckoutForm = () => {
     <form onSubmit={handleSubmit}>
       <CustomDonationInput
         className="checkout-style"
-        name={'customDonation'}
+        name="customDonation"
         value={input.customDonation}
         min={config.MIN_AMOUNT}
         max={config.MAX_AMOUNT}
