0081fc41d72dc57f6912348a48c33f492c7a4bae
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-02-20 11:22:13 -0500

    new refactor

--- pages/result.tsx
+++ pages/result.tsx
@@ -1,19 +1,14 @@
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
-
 import Layout from '../components/Layout';
 import PrintObject from '../components/PrintObject';
 import Cart from '../components/Cart';
 import ClearCart from '../components/ClearCart';
-
 import { fetchGetJSON } from '../utils/api-helpers';
 import useSWR from 'swr';
 
 const ResultPage: NextPage = () => {
   const router = useRouter();
-
-  // Fetch CheckoutSession from static page via
-  // https://nextjs.org/docs/basic-features/data-fetching#static-generation
   const { data, error } = useSWR(
     router.query.session_id
       ? `/api/checkout_sessions/${router.query.session_id}`
