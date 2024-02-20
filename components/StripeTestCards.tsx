aa0bf4aa3df315b2ee8f9bdf206a206975966aae
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-02-20 11:22:19 -0500

    new refactor

--- components/StripeTestCards.tsx
+++ components/StripeTestCards.tsx
@@ -1,19 +1,17 @@
+import React from "react";
+
+const testCardsLink = "https://stripe.com/docs/testing#cards";
+const cardNumber = "4242 4242 4242 4242";
+
 const StripeTestCards = () => {
   return (
     <div className="test-card-notice">
-      Use any of the{' '}
-      <a
-        href="https://stripe.com/docs/testing#cards"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      Use any of the{" "}
+      <a href={testCardsLink} target="_blank" rel="noopener noreferrer">
         Stripe test cards
-      </a>{' '}
-      for this demo, e.g.{' '}
-      <div className="card-number">
-        4242<span></span>4242<span></span>4242<span></span>4242
-      </div>
-      .
+      </a>{" "}
+      for this demo, e.g.{" "}
+      <div className="card-number">{cardNumber}</div>.
     </div>
   );
 };
