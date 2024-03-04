bb97c59b7cab4ce515aa5672d4555409d105eadd
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-03-04 13:52:22 -0500

    comment

--- components/PrintObject.tsx
+++ components/PrintObject.tsx
@@ -1,12 +1,22 @@
+// Import the React library
 import React from 'react';
 
+// Define the type for the props that this component accepts
 type Props = {
+  // The content prop is an object
   content: object;
 };
 
+// Define the PrintObject functional component, which is a generic
+// React component that accepts props of type Props
 const PrintObject: React.FC<Props> = ({ content }) => {
+  // Convert the content object into a formatted string using JSON.stringify()
   const formattedContent = JSON.stringify(content, null, 2);
+
+  // Return a <pre> element that contains the formatted content
   return <pre>{formattedContent}</pre>;
 };
 
+// Export the PrintObject component as the default export of this module
 export default PrintObject;
+
