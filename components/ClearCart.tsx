c452590b7f9985ceb7951ab36e3d60e71916c459
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-03-04 13:52:23 -0500

    comment

--- components/ClearCart.tsx
+++ components/ClearCart.tsx
@@ -1,12 +1,16 @@
-import { useEffect } from 'react';
-import { useShoppingCart } from 'use-shopping-cart';
+import { useEffect } from 'react'; // Importing the useEffect hook from React
+import { useShoppingCart } from 'use-shopping-cart'; // Importing the useShoppingCart hook from the use-shopping-cart package
 
+// Defining the ClearCart component
 export default function ClearCart() {
-  const { clearCart } = useShoppingCart();
+  const { clearCart } = useShoppingCart(); // Getting the clearCart function from the useShoppingCart hook
 
+  // Using the useEffect hook to clear the cart when the component mounts
   useEffect(() => {
-    clearCart();
-  }, [clearCart]);
+    clearCart(); // Calling the clearCart function
+  }, [clearCart]); // Adding clearCart as a dependency for the useEffect hook
 
+  // Returning a paragraph element indicating that the cart has been cleared
   return <p>Cart cleared.</p>;
 }
+
