d7a6176b50936a40a00f0db8258cec9242c5fefc
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-03-04 13:52:24 -0500

    comment

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -1,16 +1,22 @@
+// Import necessary modules and types from 'react' and 'use-shopping-cart'
 import React, { ReactNode } from 'react';
 import { CartProvider } from 'use-shopping-cart';
+
+// Import getStripe utility function for working with Stripe payments
 import getStripe from '../utils/get-stripe';
+
+// Import configuration object for setting up the application's currency
 import * as config from '../config';
 
 const Cart = ({ children }: { children: ReactNode }) => (
-  <CartProvider
-    mode="checkout-session"
-    stripe={getStripe()}
-    currency={config.CURRENCY}
+  <CartProvider // Create a CartProvider to manage the shopping cart state
+    mode="checkout-session" // Set the mode to 'checkout-session'
+    stripe={getStripe()} // Inject the Stripe instance created by the 'getStripe' utility function
+    currency={config.CURRENCY} // Set the currency based on the configuration object
   >
-    <>{children}</>
+    <>{children}</> {/* Render the children passed to the Cart component */}
   </CartProvider>
 );
 
+// Export the Cart component for use in other parts of the application
 export default Cart;
