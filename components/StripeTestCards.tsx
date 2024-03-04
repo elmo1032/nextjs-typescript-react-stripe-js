044046b20d6715ec0c78a1a7dbd5927b3209e391
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-03-04 13:52:21 -0500

    comment

--- components/StripeTestCards.tsx
+++ components/StripeTestCards.tsx
@@ -1,19 +1,7 @@
 import React from "react";
 
+// Save the Stripe test cards link as a constant
 const testCardsLink = "https://stripe.com/docs/testing#cards";
-const cardNumber = "4242 4242 4242 4242";
 
-const StripeTestCards = () => {
-  return (
-    <div className="test-card-notice">
-      Use any of the{" "}
-      <a href={testCardsLink} target="_blank" rel="noopener noreferrer">
-        Stripe test cards
-      </a>{" "}
-      for this demo, e.g.{" "}
-      <div className="card-number">{cardNumber}</div>.
-    </div>
-  );
-};
-
-export default StripeTestCards;
+// Save a test card number as a constant
+const cardNumber = "4242 4242 4
