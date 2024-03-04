60e19a295d5c7203b74efaa8062c846310b4f538
elmo1032 <101438336+elmo1032@users.noreply.github.com>
2024-03-04 13:52:25 -0500

    comment

--- pages/donate-with-checkout.tsx
+++ pages/donate-with-checkout.tsx
@@ -1,17 +1,18 @@
 import { NextPage } from 'next';
-import Layout from '../components/Layout';
-import CheckoutForm from '../components/CheckoutForm';
+import Layout from '../components/Layout'; // Importing the layout component
+import CheckoutForm from '../components/CheckoutForm'; // Importing the checkout form component
 
-const DonatePage: NextPage = () => {
+const DonatePage: NextPage = () => { // Defining the DonatePage functional component
   return (
-    <Layout title="Donate with Checkout | Next.js + TypeScript Example">
+    <Layout title="Donate with Checkout | Next.js + TypeScript Example"> // Using the Layout component with a given title
       <div className="page-container">
-        <h1>Donate with Checkout</h1>
-        <p>Donate to our project, ð</p>
-        <CheckoutForm />
+        <h1>Donate with Checkout</h1> {/* Page heading */}
+        <p>Donate to our project, ð</p> {/* Description to encourage users to donate */}
+        <CheckoutForm /> {/* Rendering the CheckoutForm component */}
       </div>
     </Layout>
   );
 };
 
-export default DonatePage;
+export default DonatePage; // Exporting the DonatePage component for use in other files
+
